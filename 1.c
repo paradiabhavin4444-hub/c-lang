fdfe26c8d22d7debbd6c236fc2285d91d07c77d3
paradiabhavin4444-hub <paradiabhavin4444@gmail.com>
2025-09-02 11:02:56 +0530

    second message

--- 1.c
+++ 1.c
@@ -5,6 +5,9 @@ int main()
     float c,result;
     printf("The temperature in celsius: ");
     scanf("%f",&c);
+
+
+    
     result=(9*c/5)+32;
     printf("The Temperature in fahrenheit= %.4f",result);
     return 0;
